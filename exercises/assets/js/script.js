6d9cef489ead2d033c4336ad7b174be07320c096
Jim Pioche <91530212+VictorAgahi@users.noreply.github.com>
2024-01-24 10:24:18 +0000

    fixxx

--- exercises/assets/js/script.js
+++ exercises/assets/js/script.js
@@ -10,5 +10,27 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
+
+
+
 const hiddenElement = document.querySelectorAll('.reveal-p');
 hiddenElement.forEach((el)=>observer.observe(el));
+
+
+
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal-button1-visible")
+        }
+        else
+        {
+            entry.target.classList.remove("reveal-button1-visible")
+        }
+    });
+});
+
+const hidden_Page2 = document.querySelectorAll('.reveal-button1');
+hidden_Page2.forEach((el)=>observer2.observe(el));
