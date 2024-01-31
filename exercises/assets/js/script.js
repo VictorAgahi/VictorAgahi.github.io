cbafdb0f8a3e618745e4ac0bc2a3f062a31d1c78
Jim Pioche <91530212+VictorAgahi@users.noreply.github.com>
2024-01-31 11:54:33 +0000

    fix

--- exercises/assets/js/script.js
+++ exercises/assets/js/script.js
@@ -34,3 +34,36 @@ const observer2 = new IntersectionObserver((entries) => {
 
 const hidden_Page2 = document.querySelectorAll('.reveal-button1');
 hidden_Page2.forEach((el)=>observer2.observe(el));
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal-button2-visible")
+        }
+        else
+        {
+            entry.target.classList.remove("reveal-button2-visible")
+        }
+    });
+});
+
+const hidden_Page3 = document.querySelectorAll('.reveal-button2');
+hidden_Page3.forEach((el)=>observer3.observe(el));
+
+
+const observer4 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal-button3-visible")
+        }
+        else
+        {
+            entry.target.classList.remove("reveal-button3-visible")
+        }
+    });
+});
+
+const hidden_Page4 = document.querySelectorAll('.reveal-button3');
+hidden_Page4.forEach((el)=>observer4.observe(el));
